baab3be4c19360c926f5790a12607f1982605593
Job <Jobsjobbema@gmail.com>
2026-03-29 19:13:18 +0200

    Forms uitgebreid met leeftijd geslacht en bio

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField
+from wtforms import EmailField, PasswordField, SubmitField, IntegerField, SelectField, TextAreaField
 from wtforms.validators import InputRequired, Length, EqualTo
 
 class LoginForm(FlaskForm):
@@ -11,6 +11,17 @@ class RegistratieForm(FlaskForm):
     email = EmailField('Email')
     password = PasswordField('Wachtwoord')
     confirm_password = PasswordField('Herhaal wachtwoord', validators=[EqualTo('password', message='Wachtwoorden moeten overeenkomen!')])
+    leeftijd = IntegerField('Leeftijd')
+    geslacht = SelectField(
+        'Geslacht',
+        choices=[
+            ('man', 'Man'),
+            ('vrouw', 'Vrouw'),
+            ('anders', 'Anders')
+        ]
+    )
+    bio = TextAreaField('Bio')
+
     submit = SubmitField('Registreren', render_kw={"class":"btn btn-success"})
 
 
